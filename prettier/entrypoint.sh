9ab60a73feaa5710f26154f12d845256f0ff6f1a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-03-07 14:53:41 -0500

    prettier: fix commit-options handling

--- prettier/entrypoint.sh
+++ prettier/entrypoint.sh
@@ -269,7 +269,7 @@ else
   if [ "$INPUT_COMMIT_DESCRIPTION" != "" ]; then
     git commit -n -m "$INPUT_COMMIT_MESSAGE" -m "$INPUT_COMMIT_DESCRIPTION" ${INPUT_COMMIT_OPTIONS:+$INPUT_COMMIT_OPTIONS} || commit_canary=
   else
-    git commit -n -m "$INPUT_COMMIT_MESSAGE" ${INPUT_COMMIT_OPTIONS:+"INPUT_COMMIT_OPTIONS} || commit_canary=
+    git commit -n -m "$INPUT_COMMIT_MESSAGE" ${INPUT_COMMIT_OPTIONS:+$INPUT_COMMIT_OPTIONS} || commit_canary=
   fi
 
   dry_run_check
